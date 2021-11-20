71c0659a81fa264a7f8fb0a84a083432f73eadf0
masayoshi64 <sho32001025@gmail.com>
2021-11-20 20:35:57 +0900

    add alphabeta

--- alpha_zero/game.py
+++ alpha_zero/game.py
@@ -75,3 +75,30 @@ def random_action(state):
 def argmax(collection, key=None):
     return collection.index(max(collection))
 
+
+def alpha_beta(state, alpha, beta):
+    if state.is_lose():
+        return -1
+
+    if state.is_draw():
+        return 0
+
+    for action in state.legal_actions():
+        score = -alpha_beta(state.next(action), -beta, -alpha)
+        if score > alpha:
+            alpha = score
+        if alpha >= beta:
+            return alpha
+    return alpha
+
+
+def alpha_beta_action(state):
+    best_action = 0
+    best_score = -float('inf')
+    alpha = -float('inf')
+    for action in state.legal_actions():
+        score = -alpha_beta(state.next(action), -float('inf'), -alpha)
+        if score > best_score:
+            best_score = score
+            best_action = action
+    return best_action
